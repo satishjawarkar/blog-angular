775817aa2f6f6a1102b51b603cb4e6d17c1ba7f6
satishjawarkar <jawarkar1992@gmail.com>
2021-06-11 22:10:57 +0530

    add some code changes

--- src/app/componts/home/home.component.ts
+++ src/app/componts/home/home.component.ts
@@ -125,23 +125,23 @@ export class HomeComponent implements OnInit {
 //         }
 //     }
 // });
-let ctx:any = document.getElementById('divChart');
-var chart = new Chart(ctx, {
-  type: 'line',
-  data: {
-      datasets: [{
-          data: [0, 0],
-      }, {
-          data: [0, 1]
-      }, {
-          data: [1, 0],
-          showLine: true // overrides the `line` dataset default
-      }, {
-          type: 'scatter', // 'line' dataset default does not affect this dataset since it's a 'scatter'
-          data: [1, 1]
-      }]
-  }
-});
+// let ctx:any = document.getElementById('divChart');
+// var chart = new Chart(ctx, {
+//   type: 'line',
+//   data: {
+//       datasets: [{
+//           data: [0, 0],
+//       }, {
+//           data: [0, 1]
+//       }, {
+//           data: [1, 0],
+//           showLine: true // overrides the `line` dataset default
+//       }, {
+//           type: 'scatter', // 'line' dataset default does not affect this dataset since it's a 'scatter'
+//           data: [1, 1]
+//       }]
+//   }
+// });
 
     // myChartclose
     // defult value show 
